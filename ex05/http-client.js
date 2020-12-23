fb619a5454e2ede2fe37777632d2140bb2623ea4
dnakano <dnakano@student.42tokyo.jp>
2020-12-23 20:39:27 +0900

    Update: Refactored

--- ex05/http-client.js
+++ ex05/http-client.js
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 18:30:01 by dnakano           #+#    #+#             */
-/*   Updated: 2020/12/23 19:33:55 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/12/23 20:38:22 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,11 @@ const http = require("http");
 try {
   if (process.argv[2] === undefined)
     throw new Error('Empty URL');
-  req = http.request(process.argv[2], (res) => {
+  http.request(process.argv[2], (res) => {
     res.on('data', (chunk) => {
         console.log(`${chunk}`);
     });
-  });
-  req.end();
+  }).end();
 } catch (error) {
   console.error(error.message);
 }
