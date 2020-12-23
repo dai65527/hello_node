e1ea583e3baf37b1939e5afc94a280825c15ec45
dnakano <dnakano@student.42tokyo.jp>
2020-12-23 16:47:00 +0900

    Add: ex01/vars.js

---
+++ ex01/vars.js
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vars.js                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/23 16:22:32 by dnakano           #+#    #+#             */
+/*   Updated: 2020/12/23 16:45:22 by dnakano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// string
+const fortyTwoString = "42";
+console.log(`${fortyTwoString} is a ${typeof fortyTwoString}.`);
+
+// number
+const fortyTwoNumber = 42;
+console.log(`${fortyTwoNumber} is a ${typeof fortyTwoNumber}.`);
+
+// array
+const fortyTwoArray = [42];
+console.log(`${fortyTwoArray} is an ${typeof fortyTwoArray}.`);
+
+// object
+const fortyTwoObject = {
+  fortyTwo: 42
+};
+console.log(`${fortyTwoObject} is an ${typeof fortyTwoObject}.`);
+
+// boolean
+const fortyTwoBoolean = true;
+console.log(`${fortyTwoBoolean} is a ${typeof fortyTwoBoolean}.`);
+
+// undefined
+const fortyTwoUndefined = undefined;
+console.log(`${fortyTwoUndefined} is an ${typeof fortyTwoUndefined}.`);
