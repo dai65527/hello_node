8873c129f1b1bd53ca84392374778482333a4170
dnakano <dnakano@student.42tokyo.jp>
2020-12-30 08:57:44 +0900

    Refactoring

--- ex03/io.js
+++ ex03/io.js
@@ -6,26 +6,24 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 17:52:46 by dnakano           #+#    #+#             */
-/*   Updated: 2020/12/23 19:49:30 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/12/30 08:56:38 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 const fs = require("fs");
 
 const countNewLine = (buf) => {
-    let count = 0;
-    for (let i = 0; i < buf.length; i++) {
-      if (buf[i] === "\n") {
-        count++;
-      }
+  let count = 0;
+  for (let i = 0; i < buf.length; i++) {
+    if (buf[i] === "\n") {
+      count++;
     }
-    return count;
+  }
+  return count;
 }
 
 try {
-  const buf = fs.readFileSync(process.argv[2], "utf-8");
-  const count = countNewLine(buf);
-  console.log(count);
+  console.log(countNewLine(fs.readFileSync(process.argv[2], "utf-8")));
 } catch (error) {
   console.log(error.message);  
 }
