156dbc774f08ed6d979f340d2f9c901ccdf0b426
dnakano <dnakano@student.42tokyo.jp>
2020-12-30 08:58:05 +0900

    Fix: readFileSync to readFile

--- ex04/asyncio.js
+++ ex04/asyncio.js
@@ -6,30 +6,26 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 17:52:46 by dnakano           #+#    #+#             */
-/*   Updated: 2020/12/23 19:48:52 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/12/30 08:56:32 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 const fs = require("fs");
 
 const countNewLine = (buf) => {
-    let count = 0;
-    for (let i = 0; i < buf.length; i++) {
-      if (buf[i] === "\n") {
-        count++;
-      }
+  let count = 0;
+  for (let i = 0; i < buf.length; i++) {
+    if (buf[i] === "\n") {
+      count++;
     }
-    return count;
-}
-
-const readFileCountNewLine = async (filename) => {
-  try {
-    const buf = await fs.readFileSync(filename, "utf-8");
-    const count = countNewLine(buf);
-    console.log(count);
-  } catch (error) {
-    console.log(error.message);  
   }
+  return count;
 }
 
-readFileCountNewLine(process.argv[2]);
+fs.readFile(process.argv[2], (error, data) => {
+  if (error) {
+    console.error(error.message);
+  } else {
+    console.log(countNewLine(data.toString()));
+  }
+});
