360c45e9d9d73e7208e2a85c62f9a6e4d4c2bd7a
dnakano <dnakano@student.42tokyo.jp>
2020-12-30 15:26:20 +0900

    Refactoring

--- ex06/http-collect.js
+++ ex06/http-collect.js
@@ -6,31 +6,34 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 19:51:26 by dnakano           #+#    #+#             */
-/*   Updated: 2020/12/24 17:42:39 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/12/30 15:23:54 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 const http = require("http");
 
 try {
-  if (process.argv[2] === undefined)
-    throw new Error('Empty URL');
-  http.request(process.argv[2], (res) => {
-    res.setEncoding('utf8');
+  if (process.argv.length != 3) {
+    throw new Error("Error: invalid argument");
+  }
+  const req = http.request(process.argv[2], (res) => {
+    res.setEncoding("utf-8");
     let dataString = "";
-    res.on("error", (error) => {
-      console.error(`error code: ${error.code}`);
-    });
     res.on("data", (chunk) => {
       dataString += chunk.toString();
+    });
+    res.on("error", (error) => {
+      console.error("Error on response: " + error.code);
     });
     res.on("end", () => {
       console.log(dataString.length);
       console.log(dataString);
     });
-  }).on("error", (error) => {
-    console.error(`error code: ${error.code}`);
-  }).end();
+  });
+  req.on("error", (error) => {
+    console.error("Error on request: " + error.code);
+  });
+  req.end();
 } catch (error) {
   console.error(error.message);
 }
